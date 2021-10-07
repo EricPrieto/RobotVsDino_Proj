627efdd880018725a08c6b7ea2b18777e79cc3d4
EricPrieto <ep1043@hotmail.com>
2021-10-07 11:31:09 -0500

    Created list to store Dinosaur name and power

--- herd.py
+++ herd.py
@@ -5,5 +5,10 @@ class Herd:
         self.dinosaurs = []
 
         
-    def dino_herd(self):
-        pass
+    def create_herd(self):
+        dino1 = Dinosaur("Deno",20)
+        dino2 = Dinosaur("Lizardo", 25)
+        dino3 = Dinosaur("Godzilla", 40)
+        self.dinosaurs.append(dino1)
+        self.dinosaurs.append(dino2)
+        self.dinosaurs.append(dino1)
